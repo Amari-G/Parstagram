8bd1004644a51586e7ae5cb24c5ec89233f1234b
Amari Garrett <amarig@fb.com>
2020-07-09 01:15:37 -0400

    - Display Toolbar with app icon in home screen

--- app/src/main/java/com/example/parstagram/MainActivity.java
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.parstagram;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.parstagram.databinding.ActivityMainBinding;
@@ -31,6 +33,27 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView = binding.bottomNavigationView;
         setBottomNavItemSelectedListener();
+
+        // Find the toolbar view and set as ActionBar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.mainToolbar);
+        setSupportActionBar(toolbar);
+        // ...
+        // Display icon in the toolbar
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.nav_logo_whiteout);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.nav_camera);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.feed_toolbar_menu, menu);
+        return true;
     }
 
     public void replaceFragment(Fragment fragment) {
